1fb2005521e03afe9f2a85894c5736fa75c9b200
Rasmus Wejlgaard <a-rwejlgaard@expediagroup.com>
2024-01-05 16:39:11 +0000

    add jellyfin endpoint

--- cloudflare_tunnels.tf
+++ cloudflare_tunnels.tf
@@ -1,89 +1,89 @@
 resource "cloudflare_tunnel" "london-a" {
-    account_id = cloudflare_account.this.id
-    name = "London A"
-    secret = ""
+  account_id = cloudflare_account.this.id
+  name       = "London A"
+  secret     = ""
 }
 
 resource "cloudflare_tunnel" "london-b" {
-   account_id = cloudflare_account.this.id
-   name = "London B"
-   secret = ""
+  account_id = cloudflare_account.this.id
+  name       = "London B"
+  secret     = ""
 }
 
 resource "cloudflare_tunnel" "copenhagen-a" {
-   account_id = cloudflare_account.this.id
-   name = "Copenhagen A"
-   secret = ""
+  account_id = cloudflare_account.this.id
+  name       = "Copenhagen A"
+  secret     = ""
 }
 
 resource "cloudflare_tunnel" "copenhagen-b" {
-   account_id = cloudflare_account.this.id
-   name = "Copenhagen B"
-   secret = ""
+  account_id = cloudflare_account.this.id
+  name       = "Copenhagen B"
+  secret     = ""
 }
 
 resource "cloudflare_tunnel" "copenhagen-c" {
-   account_id = cloudflare_account.this.id
-   name = "Copenhagen C"
-   secret = ""
+  account_id = cloudflare_account.this.id
+  name       = "Copenhagen C"
+  secret     = ""
 }
 
 resource "cloudflare_tunnel_config" "london-a" {
   account_id = cloudflare_account.this.id
-  tunnel_id = cloudflare_tunnel.london-a.id
+  tunnel_id  = cloudflare_tunnel.london-a.id
   config {
     ingress_rule {
       hostname = "grafana.pez.sh"
-      service = "http://localhost:3000"
+      service  = "http://localhost:3000"
       origin_request {
         access {
-          aud_tag = [ cloudflare_access_application.grafana.aud ]
-          required = true
+          aud_tag   = [cloudflare_access_application.grafana.aud]
+          required  = true
           team_name = "pezsh"
         }
       }
     }
     ingress_rule {
       hostname = "portainer.pez.sh"
-      service = "https://localhost:9443"
+      service  = "https://localhost:9443"
       origin_request {
         access {
-          aud_tag = [ cloudflare_access_application.portainer.aud ]
-          required = true
-          team_name = "pezsh" 
+          aud_tag   = [cloudflare_access_application.portainer.aud]
+          required  = true
+          team_name = "pezsh"
         }
         no_tls_verify = true
       }
     }
     ingress_rule {
       hostname = "prometheus.pez.sh"
-      service = "http://localhost:9090"
+      service  = "http://localhost:9090"
       origin_request {
         access {
-          aud_tag = [ cloudflare_access_application.prometheus.aud ]
-          required = true
-          team_name = "pezsh" 
+          aud_tag   = [cloudflare_access_application.prometheus.aud]
+          required  = true
+          team_name = "pezsh"
         }
       }
     }
     ingress_rule {
       hostname = "alertmanager.pez.sh"
-      service = "http://localhost:9093"
+      service  = "http://localhost:9093"
       origin_request {
         access {
-          aud_tag = [ cloudflare_access_application.alertmanager.aud ]
-          required = true
-          team_name = "pezsh" 
+          aud_tag   = [cloudflare_access_application.alertmanager.aud]
+          required  = true
+          team_name = "pezsh"
         }
       }
     }
     ingress_rule {
       hostname = "minecraft-map.pez.sh"
-      service = "http://localhost:8124"
+      service  = "http://localhost:8124"
     }
     ingress_rule {
       hostname = "minecraft.pez.sh"
-      service = "tcp://localhost:25565"
+      service  = "tcp://localhost:25565"
     }
     ingress_rule {
       service = "http_status:404"
@@ -93,15 +93,15 @@ resource "cloudflare_tunnel_config" "london-a" {
 
 resource "cloudflare_tunnel_config" "london-b" {
   account_id = cloudflare_account.this.id
-  tunnel_id = cloudflare_tunnel.london-b.id
+  tunnel_id  = cloudflare_tunnel.london-b.id
   config {
     ingress_rule {
       hostname = "radarr.pez.sh"
-      service = "http://192.168.1.253:7878"
+      service  = "http://192.168.1.253:7878"
       origin_request {
         access {
-          aud_tag = [ cloudflare_access_application.radarr.aud ]
-          required = true
+          aud_tag   = [cloudflare_access_application.radarr.aud]
+          required  = true
           team_name = "pezsh"
         }
         http_host_header = "localhost:7878"
@@ -109,77 +109,81 @@ resource "cloudflare_tunnel_config" "london-b" {
     }
     ingress_rule {
       hostname = "download.pez.sh"
-      service = "http://localhost:9091"
+      service  = "http://localhost:9091"
       origin_request {
         access {
-          aud_tag = [ cloudflare_access_application.transmission.aud ]
-          required = true
-          team_name = "pezsh" 
+          aud_tag   = [cloudflare_access_application.transmission.aud]
+          required  = true
+          team_name = "pezsh"
         }
         http_host_header = "localhost:9091"
       }
     }
     ingress_rule {
       hostname = "sonarr.pez.sh"
-      service = "http://localhost:8989"
+      service  = "http://localhost:8989"
       origin_request {
         access {
-          aud_tag = [ cloudflare_access_application.sonarr.aud ]
-          required = true
-          team_name = "pezsh" 
+          aud_tag   = [cloudflare_access_application.sonarr.aud]
+          required  = true
+          team_name = "pezsh"
         }
         http_host_header = "localhost:8989"
       }
     }
     ingress_rule {
       hostname = "prowlarr.pez.sh"
-      service = "http://localhost:9696"
+      service  = "http://localhost:9696"
       origin_request {
         access {
-          aud_tag = [ cloudflare_access_application.prowlarr.aud ]
-          required = true
-          team_name = "pezsh" 
+          aud_tag   = [cloudflare_access_application.prowlarr.aud]
+          required  = true
+          team_name = "pezsh"
         }
         http_host_header = "prowlarr.pez.sh"
       }
     }
     ingress_rule {
       hostname = "plex.pez.sh"
-      service = "http://localhost:32400"
+      service  = "http://localhost:32400"
       origin_request {
         access {
-          aud_tag = [ cloudflare_access_application.plex.aud ]
-          required = true
-          team_name = "pezsh" 
+          aud_tag   = [cloudflare_access_application.plex.aud]
+          required  = true
+          team_name = "pezsh"
         }
         http_host_header = "plex.pez.sh"
       }
     }
     ingress_rule {
       hostname = "overseer.pez.sh"
-      service = "http://localhost:5055"
+      service  = "http://localhost:5055"
       origin_request {
         access {
-          aud_tag = [ cloudflare_access_application.request.aud ]
-          required = true
-          team_name = "pezsh" 
+          aud_tag   = [cloudflare_access_application.request.aud]
+          required  = true
+          team_name = "pezsh"
         }
       }
     }
     ingress_rule {
       hostname = "lidarr.pez.sh"
-      service = "http://localhost:8686"
+      service  = "http://localhost:8686"
       origin_request {
         access {
-          aud_tag = [ cloudflare_access_application.lidarr.aud ]
-          required = true
-          team_name = "pezsh" 
+          aud_tag   = [cloudflare_access_application.lidarr.aud]
+          required  = true
+          team_name = "pezsh"
         }
       }
     }
     ingress_rule {
       hostname = "request.pez.sh"
-      service = "http://localhost:5055"
+      service  = "http://localhost:5055"
+    }
+    ingress_rule {
+      hostname = "jellyfin.pez.sh"
+      service  = "http://localhost:8096"
     }
     ingress_rule {
       service = "http_status:404"
@@ -189,7 +193,7 @@ resource "cloudflare_tunnel_config" "london-b" {
 
 resource "cloudflare_tunnel_config" "copenhagen-a" {
   account_id = cloudflare_account.this.id
-  tunnel_id = cloudflare_tunnel.copenhagen-a.id
+  tunnel_id  = cloudflare_tunnel.copenhagen-a.id
   config {
     ingress_rule {
       service = "http_status:404"
@@ -199,8 +203,16 @@ resource "cloudflare_tunnel_config" "copenhagen-a" {
 
 resource "cloudflare_tunnel_config" "copenhagen-b" {
   account_id = cloudflare_account.this.id
-  tunnel_id = cloudflare_tunnel.copenhagen-b.id
+  tunnel_id  = cloudflare_tunnel.copenhagen-b.id
   config {
+    ingress_rule {
+      hostname = "cloud.pez.sh"
+      service  = "http://localhost:11000"
+      origin_request {
+        no_tls_verify      = true
+        origin_server_name = "cloud.pez.sh"
+      }
+    }
     ingress_rule {
       service = "http_status:404"
     }
@@ -209,7 +221,7 @@ resource "cloudflare_tunnel_config" "copenhagen-b" {
 
 resource "cloudflare_tunnel_config" "copenhagen-c" {
   account_id = cloudflare_account.this.id
-  tunnel_id = cloudflare_tunnel.copenhagen-c.id
+  tunnel_id  = cloudflare_tunnel.copenhagen-c.id
   config {
     ingress_rule {
       service = "http_status:404"
